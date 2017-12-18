dff108e4c963ee80cd72fa306dc60260d6b0b374
Harmin Jeong <harminjeong@Harmins-MacBook-Pro.local>
2017-12-18 09:45:04 -0500

    works

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,10 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    var movies=["The Matrix", "The Departed", "Inception", "The Godfather", "Gladiator", "Interstellar", "Saving Private Ryan"]
-    
-    function renderButtons(){
+    var movies = ["The Matrix", "The Departed", "Inception", "The Godfather", "Gladiator", "Interstellar", "Saving Private Ryan"]
 
+    function renderButtons() {
         $(".movieButtons").empty();
-
-        for (var i = 0; i < movies.length; i++){
+        for (var i = 0; i < movies.length; i++) {
             var a = $("<button>");
             a.addClass("movie");
             a.attr("data-name", movies[i]);
@@ -14,34 +12,40 @@ $(document).ready(function(){
             $(".movieButtons").append(a);
         }
     }
+    function displayMovieGif() {
 
-    $("#add-movie").on("click", function(event){
         var movie = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=90yEQeZ9Y39imuC6eO1llo4Gx9NaVZMm&limit=5";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=90yEQeZ9Y39imuC6eO1llo4Gx9NaVZMm&q=" + movie + "&limit=10offset=0&rating=G&lang=en";
 
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).done(function(response){
+        }).done(function (response) {
             console.log(queryURL);
-            console.log(response)
-            var movieUrl = response.data.image_original_url;
-            var movieImage = $("<img>")
-            var movies = response.data
-            movieImage.attr("src", movieUrl);
-            movieImage.attr("alt", "movie image");
+            console.log(response);
 
-            $("#images").prepend(movieImage);
+            var data = response.data;
+            for (var i = 0; i < data.length; i++) {
+                var movieUrl = data[i].images.fixed_height.url;
+                $("<div>").append("#images")
+                var movieImage = $("<img>")
+                // var movies = response.data
+                movieImage.attr("src", movieUrl);
+                movieImage.attr("alt", "movie image");
+                $("#images").append(movieImage);
+            }
         })
+    }
+    $("#add-movie").on("click", function (event) {
+
         event.preventDefault();
         var movie = $("#movie-input").val().trim();
         movies.push(movie);
         renderButtons();
 
     })
+    $(document).on("click", ".movie", displayMovieGif);
     renderButtons();
 
-    // function displayMovieGif(){
 
-    // }
 })
